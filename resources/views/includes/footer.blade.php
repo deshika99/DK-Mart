167b57259b3c990b5ab3a8da8b1177cdc1adca1f
Kasuni Gayanthika <gayanthikakasuni3@gmail.com>
2024-10-25 18:03:33 +0530

    create navbar blade & footer blade edit them

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,142 @@
+
+<!-- ==================== Footer Two Start Here ==================== -->
+<footer class="footer py-80 overflow-hidden">
+    <div class="container container-lg">
+        <div class="footer-item-two-wrapper d-flex align-items-start flex-wrap">
+            <div class="footer-item max-w-275" data-aos="fade-up" data-aos-duration="200">
+                <div class="footer-item__logo">
+                    <a href="index.html"> <img src="frontend/assets/images/logo/logo-two-black.png" alt=""></a>
+                </div>
+                <p class="mb-24">Marketpro  become the largest computer parts, gaming pc parts, and other IT related products.</p>
+                <div class="flex-align gap-16 mb-16">
+                    <span class="w-32 h-32 flex-center rounded-circle border border-gray-100 text-main-two-600 text-md flex-shrink-0"><i class="ph-fill ph-phone-call"></i></span>
+                    <a href="tel:+00123456789" class="text-md text-gray-900 hover-text-main-600">+00 123 456 789</a>
+                </div>
+                <div class="flex-align gap-16 mb-16">
+                    <span class="w-32 h-32 flex-center rounded-circle border border-gray-100 text-main-two-600 text-md flex-shrink-0"><i class="ph-fill ph-envelope"></i></span>
+                    <a href="mailto:support24@marketpro.com" class="text-md text-gray-900 hover-text-main-600">support24@marketpro.com</a>
+                </div>
+                <div class="flex-align gap-16 mb-16">
+                    <span class="w-32 h-32 flex-center rounded-circle border border-gray-100 text-main-two-600 text-md flex-shrink-0"><i class="ph-fill ph-map-pin"></i></span>
+                    <span class="text-md text-gray-900 ">789 Inner Lane, California, USA</span>
+                </div>
+            </div>
+            
+            <div class="footer-item" data-aos="fade-up" data-aos-duration="400">
+                <h6 class="footer-item__title">About us</h6>
+                <ul class="footer-menu">
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Company Profile</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">All Retail Store</a>
+                    </li>
+                   
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Contact Us</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Feedback</a>
+                    </li>
+                   
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Rules & Policy</a>
+                    </li>
+                </ul>
+            </div>
+
+           
+            <div class="footer-item" data-aos="fade-up" data-aos-duration="800">
+                <h6 class="footer-item__title">My Account</h6>
+                <ul class="footer-menu">
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">My Account</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Order History</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Shoping Cart</a>
+                    </li>
+                   
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Wishlist</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Order History</a>
+                    </li>
+                   
+                </ul>
+            </div>
+
+            <div class="footer-item" data-aos="fade-up" data-aos-duration="1000">
+                <h6 class="footer-item__title">Information</h6>
+                <ul class="footer-menu">
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Become a Vendor</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Affiliate Program</a>
+                    </li>
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Privacy Policy</a>
+                    </li>
+                  
+                    <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Extended Plan</a>
+                    </li>
+                   
+                     <li class="mb-16">
+                        <a href="shop.html" class="text-gray-600 hover-text-main-600">Community</a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="footer-item" data-aos="fade-up" data-aos-duration="1200">
+                <h6 class="">Connect with us</h6>
+              
+
+                <ul class="flex-align gap-16">
+                    <li>
+                        <a href="https://www.facebook.com" class="w-44 h-44 flex-center bg-main-two-50 text-main-two-600 text-xl rounded-8 hover-bg-main-two-600 hover-text-white">
+                            <i class="ph-fill ph-facebook-logo"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com" class="w-44 h-44 flex-center bg-main-two-50 text-main-two-600 text-xl rounded-8 hover-bg-main-two-600 hover-text-white">
+                            <i class="ph-fill ph-twitter-logo"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com" class="w-44 h-44 flex-center bg-main-two-50 text-main-two-600 text-xl rounded-8 hover-bg-main-two-600 hover-text-white">
+                            <i class="ph-fill ph-instagram-logo"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.pinterest.com" class="w-44 h-44 flex-center bg-main-two-50 text-main-two-600 text-xl rounded-8 hover-bg-main-two-600 hover-text-white">
+                            <i class="ph-fill ph-linkedin-logo"></i>
+                        </a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- bottom Footer -->
+<div class="bottom-footer bg-color-three py-8">
+    <div class="container container-lg">
+        <div class="bottom-footer__inner flex-between flex-wrap gap-16 py-16">
+            <p class="bottom-footer__text wow fadeInLeftBig">DK-Mart eCommerce &copy; 2024. All Rights Reserved </p>
+            <div class="flex-align gap-8 flex-wrap wow fadeInRightBig">
+                <span class="text-heading text-sm">We Are Accepting</span>
+                <img src="frontend/assets/images/thumbs/payment-method.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ==================== Footer Two End Here ==================== -->
+  
+
+    
