93098916d6f53acf42438f32b8a41e360ed442a1
Hirushan921 <himashahiru921@gmail.com>
2024-11-01 16:46:46 +0530

    product view

--- app/Http/Controllers/HomeTemplateController.php
+++ app/Http/Controllers/HomeTemplateController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeTemplateController extends Controller
 {
     public function index(){
-        return view('frontend.Home');
+        $products = Product::with('images')->get();
+        return view('frontend.Home',compact('products'));
     }
 }
