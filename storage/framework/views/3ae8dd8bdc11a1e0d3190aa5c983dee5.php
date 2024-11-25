f6ba4b024aefc4ac6c96aefdef8c494b07647617
deshika99 <hansini199971@gmail.com>
2024-11-25 09:53:45 +0530

    update

--- storage/framework/views/3ae8dd8bdc11a1e0d3190aa5c983dee5.php
+++
@@ -1,2 +0,0 @@
-
-<?php echo $__env->make('frontend.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\DK-Mart\resources\views/frontend/Home.blade.php ENDPATH**/ ?>
